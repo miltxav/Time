526438e5bfe00edb262e3c6dd86ab9dd1bb756bc
miltxav <limaxmilton@gmail.com>
2020-07-31 16:53:35 -0300

    Primeiro commit

---
+++ src/Time/Time.java
@@ -0,0 +1,36 @@
+package Time;
+
+public class Time {
+
+	private int hora;
+	private int min;
+	private int seg;
+
+	public void setTime(int h, int m, int s) {
+		this.hora = h;
+		this.min = m;
+		this.seg = s;
+	}
+
+	// vou "sobrecarregar" o metodo setTime
+	
+	
+	
+public void setHora (int hora) {
+	this.hora = hora;
+}
+
+public void setMin (int min) {
+	this.min = min;
+}
+
+public void setSeg (int seg) {
+	this.seg = seg;
+}
+
+public void mudaPadrao (int novoPadrao) {
+	hora = hora
+
+	}
+
+}
